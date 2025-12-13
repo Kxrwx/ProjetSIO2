117ad84e68eb487948a251eb7afde81333b86b23
Kxrwx <maxime.iffland@icloud.com>
2025-12-13 15:44:32 +0100

    Ajout de tuto pour la DB et setup de la DB

--- SchemaDB/SchemaDBv2.sql
+++ SchemaDB/SchemaDBv2.sql
@@ -5,7 +5,7 @@ CREATE TABLE `users` (
   `email` varchar(255) UNIQUE NOT NULL,
   `password_hash` varchar(255) NOT NULL,
   `role_id` int NOT NULL,
-  `is_active` boolean DEFAULT true,
+  `is_active` TINYINT(1) NOT NULL DEFAULT 1,
   `last_login` datetime,
   `created_at` datetime DEFAULT (CURRENT_TIMESTAMP)
 );
@@ -15,10 +15,10 @@ CREATE TABLE `signalements` (
   `title` varchar(255) NOT NULL,
   `tracking_code` varchar(255) UNIQUE NOT NULL COMMENT 'Code unique haute entropie',
   `tracking_password_hash` varchar(255) NOT NULL COMMENT 'Mot de passe secondaire du dossier',
-  `victim_name_encrypted` varchar(50),
-  `victim_contact_encrypted` varchar(100),
+  `victim_name_encrypted` varchar(255),
+  `victim_contact_encrypted` varchar(255),
   `description_encrypted` text NOT NULL,
-  `lieu_encrypted` varchar(60),
+  `lieu_encrypted` varchar(255),
   `date_encrypted` datetime,
   `id_statut` int NOT NULL,
   `id_priorite` int NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE `permission` (
 
 CREATE TABLE `pieces_jointes` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `signalement_id` int NOT NULL,
+  `id_signalement` int NOT NULL,
   `encrypted_path` varchar(255) NOT NULL COMMENT 'Chemin S3/Local vers le fichier chiffré',
   `original_filename_encrypted` blob COMMENT 'Nom du fichier chiffré',
   `file_size` int,
@@ -64,16 +64,16 @@ CREATE TABLE `pieces_jointes` (
 
 CREATE TABLE `messages` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `signalement_id` int NOT NULL,
+  `id_signalement` int NOT NULL,
   `user_id` int COMMENT 'Null si c''''est le déposant anonyme',
   `contenu_encrypted` varchar(255) NOT NULL COMMENT 'Chiffré AES-256',
-  `is_read` boolean DEFAULT false,
+  `is_read` TINYINT(1) NOT NULL DEFAULT 0,
   `created_at` datetime DEFAULT (CURRENT_TIMESTAMP)
 );
 
 CREATE TABLE `audit_logs` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `signalement_id` int,
+  `id_signalement` int,
   `user_id` int,
   `action` varchar(255) NOT NULL,
   `details_encrypted` varchar(255),
@@ -85,7 +85,7 @@ CREATE TABLE `audit_logs` (
 
 CREATE TABLE `ia_classifications` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `signalement_id` int NOT NULL,
+  `id_signalement` int NOT NULL,
   `categorie_proposee_id` INT NOT NULL,
   `created_at` datetime DEFAULT (CURRENT_TIMESTAMP)
 );
@@ -100,16 +100,16 @@ ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `role` (`id_role`);
 
 ALTER TABLE `role` ADD FOREIGN KEY (`id_permission`) REFERENCES `permission` (`id_permission`);
 
-ALTER TABLE `pieces_jointes` ADD FOREIGN KEY (`signalement_id`) REFERENCES `signalements` (`id_signalement`);
+ALTER TABLE `pieces_jointes` ADD FOREIGN KEY (`id_signalement`) REFERENCES `signalements` (`id_signalement`);
 
-ALTER TABLE `messages` ADD FOREIGN KEY (`signalement_id`) REFERENCES `signalements` (`id_signalement`);
+ALTER TABLE `messages` ADD FOREIGN KEY (`id_signalement`) REFERENCES `signalements` (`id_signalement`);
 
 ALTER TABLE `messages` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `audit_logs` ADD FOREIGN KEY (`signalement_id`) REFERENCES `signalements` (`id_signalement`);
+ALTER TABLE `audit_logs` ADD FOREIGN KEY (`id_signalement`) REFERENCES `signalements` (`id_signalement`);
 
 ALTER TABLE `audit_logs` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `ia_classifications` ADD FOREIGN KEY (`signalement_id`) REFERENCES `signalements` (`id_signalement`);
+ALTER TABLE `ia_classifications` ADD FOREIGN KEY (`id_signalement`) REFERENCES `signalements` (`id_signalement`);
 
 ALTER TABLE `ia_classifications` ADD FOREIGN KEY (`categorie_proposee_id`) REFERENCES `categorie` (`id_categorie`);
