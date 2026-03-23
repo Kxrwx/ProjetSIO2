0b633417c168ba5d09d86be66c0436151a461679
Kxrwx <maxime.iffland@icloud.com>
2026-03-23 17:37:01 +0100

    Ajout des role et permission dans /api/admin

--- webapp-signalement/backend/src/controllers/admin/isAuth.ts
+++ webapp-signalement/backend/src/controllers/admin/isAuth.ts
@@ -9,7 +9,12 @@ export default async function isAuth(req : AuthRequest, res : Response){
         if(!userId) return res.status(404).json({error : "Aucun User"})
         const sessionId = req.session?.id
         if(!sessionId) return res.status(404).json(`Aucune session`) 
-        return res.status(200).json({authentified : true})
+        const idRole = req.role?.idRole
+        const nameRole = req.role?.nameRole
+        const idPermissions = req.permissions?.map(p => p.idPermission)
+        const namePermission = req.permissions?.map(p => p.nameRole)
+        const body = {idRole, nameRole, idPermissions, namePermission}
+        return res.status(200).json(body)
     }
     catch (error) {
         return res.status(500).json({error : "Erreur serveur"})
