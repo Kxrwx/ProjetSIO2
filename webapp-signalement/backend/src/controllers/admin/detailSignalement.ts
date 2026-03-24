7122317f3344844f9d348727038c526a43de7224
babauJRJ <bapt.wars16@gmail.com>
2026-03-24 17:41:01 +0100

    jsp

--- webapp-signalement/backend/src/controllers/admin/detailSignalement.ts
+++ webapp-signalement/backend/src/controllers/admin/detailSignalement.ts
@@ -5,7 +5,7 @@ import { getDetailSignalement } from "../../models/signalement";
 
 export default async function detailSignalement(req : AuthRequest, res : Response){
     try {
-        const idSignalement = req.body
+        const {idSignalement} = req.body
         if(!idSignalement) return res.status(404).json({error : "Aucun signalement trouvé pour cette id"})
         const detailSignalement = await getDetailSignalement(idSignalement)
         if(!detailSignalement) return res.status(404).json(`Aucun signalement trouvé pour l'id ${idSignalement}`) 
