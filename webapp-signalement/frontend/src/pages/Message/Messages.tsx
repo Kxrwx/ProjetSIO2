0cc6f479e621e6a3b93633c9e89ae1694c9d9e27
babauJRJ <bapt.wars16@gmail.com>
2026-03-26 10:04:58 +0100

    ajout page message

---
+++ webapp-signalement/frontend/src/pages/Message/Messages.tsx
@@ -0,0 +1,7 @@
+export default function Messages() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <p className="text-xl font-semibold text-gray-500">Page de messages en construction...</p>
+    </div>
+  );
+}
