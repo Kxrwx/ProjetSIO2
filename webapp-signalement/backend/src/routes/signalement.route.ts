e204a50e9a3ec0675d2ab82cdc864386e08e6fca
Kxrwx <maxime.iffland@icloud.com>
2026-03-30 16:24:35 +0200

    Create file victim

--- webapp-signalement/backend/src/routes/signalement.route.ts
+++ webapp-signalement/backend/src/routes/signalement.route.ts
@@ -4,6 +4,7 @@ import getSignalement from "../controllers/signalement/consultSignalement"
 import createMessageNoAdmin from "../controllers/signalement/createMessage"
 import multer from "multer";
 import getFileNoAdmin from "../controllers/admin/file/getFile"
+import createFileNoAdmin from "../controllers/signalement/file/createFile";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -15,6 +16,7 @@ route.post("/consult", getSignalement)
 route.post("/createMessage", createMessageNoAdmin)
 
 route.post("/file", getFileNoAdmin)
+route.post("/createFile", upload.single('file'), createFileNoAdmin)
 
 export default route
 
